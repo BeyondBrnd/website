4211def0bbd1d201a631663bfc73c61289dc3b47
Bharti <bharti@beyondbrnd.co>
2026-08-19 18:51:49 +0530

    Update layout.tsx
    LinkedIn inside tag changes made.

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,9 +52,29 @@ export default function RootLayout({
       <body
         className={`${cerebri.variable} antialiased selection:bg-[#00BF63]/30 selection:text-white`}
       >
-        <script type="text/javascript">
-_linkedin_partner_id = "10574081";
-window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+      <Script id="linkedin_insight_tag" strategy="lazyOnload">
+        {
+          `
+//           <script type="text/javascript">
+// _linkedin_partner_id = "10574081";
+// window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+// window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+// </script><script type="text/javascript">
+// (function(l) {
+// if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+// window.lintrk.q=[]}
+// var s = document.getElementsByTagName("script")[0];
+// var b = document.createElement("script");
+// b.type = "text/javascript";b.async = true;
+// b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+// s.parentNode.insertBefore(b, s);})(window.lintrk);
+// </script>
+// <noscript>
+// <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=10574081&fmt=gif" />
+// </noscript>
+// ADDED THE LINKEDIN TAG HERE, THE ORIGINAL CODE STARTS FROM 58TH LINE
+ _linkedin_partner_id = "10574081";
+ window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
 window._linkedin_data_partner_ids.push(_linkedin_partner_id);
 </script><script type="text/javascript">
 (function(l) {
@@ -65,10 +85,10 @@ var b = document.createElement("script");
 b.type = "text/javascript";b.async = true;
 b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
 s.parentNode.insertBefore(b, s);})(window.lintrk);
-</script>
-<noscript>
-<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=10574081&fmt=gif" />
-</noscript>
+          `
+        }
+        
+        </Script>
         <CustomCursor />
         {children}
       </body>
